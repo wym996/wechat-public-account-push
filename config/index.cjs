9844f543802762dfb7e96198a1c6e810ac81f464
wym996 <58722877+wym996@users.noreply.github.com>
2023-01-29 21:24:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '♥宝贝♥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'okKRn60jamSMNX4AsKeWzmD0veKU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
